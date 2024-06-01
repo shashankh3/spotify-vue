def327678f8f9be9fef725af59ea02171bf72eb7
shashank <108757657+shashankh3@users.noreply.github.com>
2024-06-01 17:21:13 +0530

    Update Main.vue

--- spotify-vue/src/pages/Main.vue
+++ spotify-vue/src/pages/Main.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="welcome">welcome to SPOTIFY</div>
+<div class="welcome">welcome to SPOTIFY</div>
+<button @click="goToAdmin">Admin</button>
 </template>
 
 <script>
+
+
 export default {
-name: 'Main'
+name: 'Main',
+methods: {
+    goToAdmin() {
+      this.$router.push('/admin');
+    }
+}
 }
 </script>
 
-<style>
-
+<style scoped > 
+button{
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: green;
+  position: absolute;
+  left: 35%;
+  width: 25%;
+}
 </style>
